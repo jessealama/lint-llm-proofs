9742e9f1c3635ce7bc13dd0f24b7917348f49246
Jesse Alama <hello@jessealama.net>
2026-01-08 14:24:54 +0100

    Add nestedLet linter

--- LintLlmProofs.lean
+++ LintLlmProofs.lean
@@ -1,6 +1,7 @@
 -- This module serves as the root of the `LintLlmProofs` library.
 -- Import modules here that should be built as part of the library.
 import LintLlmProofs.NestedHave
+import LintLlmProofs.NestedLet
 import LintLlmProofs.HaveRw
 import LintLlmProofs.SimpRfl
 import LintLlmProofs.SequentialIntros
